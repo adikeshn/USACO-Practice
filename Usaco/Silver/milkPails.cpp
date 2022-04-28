8b82d0a42abb42bd85ad368ea672dea5a5f4841b
Turtlez200 <88110937+Turtlez200@users.noreply.github.com>
2022-04-28 07:49:41 -0400

    New Problem

--- Usaco/Silver/milkPails.cpp
+++ Usaco/Silver/milkPails.cpp
@@ -3,69 +3,36 @@
 #include <algorithm>
 #include <fstream>
 using namespace std;
-bool vis[200] = {false};
-int X, Y;
-vector<int> dfs(int currdepth, int depth, vector<int> curPail, vector<int> sums)
+bool vis[101][101][101] = {false};
+int X, Y, solution = 200, K, M;
+void dfs(int currdepth, int curX, int curY)
 {
-    if (currdepth < depth)
-    {
-        if (vis[curPail[0] + curPail[1]] == false)
-        {
-            vis[curPail[0] + curPail[1]] = true;
-            sums.push_back(curPail[0] + curPail[1]);
-            int x = curPail[0], y = curPail[1];
-            vector<int> temp = curPail;
-            temp[0] = 0;
-            sums = dfs(currdepth + 1, depth, temp, sums);
-            temp[0] = x;
-            temp[1] = 0;
-            sums = dfs(currdepth + 1, depth, temp, sums);
-            temp[1] = y;
-            temp[0] += temp[1];
-            if (temp[0] > X)
-            {
-                temp[1] -= temp[0] - X;
-                temp[0] = X;
-            }
-            else
-            {
-                temp[1] = 0;
-            }
-            sums = dfs(currdepth + 1, depth, temp, sums);
-            temp[0] = x;
-            temp[1] = y;
-            temp[1] += temp[0];
-            if (temp[1] > Y)
-            {
-                temp[0] -= temp[1] - Y;
-                temp[1] = Y;
-            }
-            else
-            {
-                temp[0] = 0;
-            }
-            sums = dfs(currdepth + 1, depth, temp, sums);
-            temp[0] = X;
-            temp[1] = y;
-            sums = dfs(currdepth + 1, depth, temp, sums);
-            temp[0] = x;
-            temp[1] = Y;
-            sums = dfs(currdepth + 1, depth, temp, sums);
-        }
-    }
-    return sums;
+    if (currdepth > K || vis[curX][curY][currdepth])
+        return;
+    vis[curX][curY][currdepth] = true;
+    solution = min(solution, abs(M - (curX + curY)));
+    dfs(currdepth + 1, 0, curY);
+    dfs(currdepth + 1, curX, 0);
+
+    dfs(currdepth + 1, X, curY);
+    dfs(currdepth + 1, curX, Y);
+
+    if (curX + curY > X)
+        dfs(currdepth + 1, X, curY - (X - curX));
+    else
+        dfs(currdepth + 1, curX + curY, 0);
+
+    if (curX + curY > Y)
+        dfs(currdepth + 1, curX - (Y - curY), Y);
+    else
+        dfs(currdepth + 1, 0, curX + curY);
 }
 
 int main()
 {
     ifstream fin("pails.in");
     ofstream fout("pails.out");
-    int K, M;
     fin >> X >> Y >> K >> M;
-    vector<int> curPail(2, 0), i, ans;
-    ans = dfs(0, K, curPail, i);
-    for (int x = 0; x < ans.size(); x++)
-    {
-        cout << x << endl;
-    }
+    dfs(0, 0, 0);
+    fout << solution << endl;
 }
