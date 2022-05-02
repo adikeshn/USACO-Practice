3688eb36848e1cf4b62f361c581a0ef64009133f
Turtlez200 <88110937+Turtlez200@users.noreply.github.com>
2022-05-02 14:16:59 -0400

    Update angryCow.cpp

--- Usaco/Silver/angryCow.cpp
+++ Usaco/Silver/angryCow.cpp
@@ -4,34 +4,57 @@
 #include <vector>
 using namespace std;
 
-int main(){
+bool checkTrue(int x, vector<int> bales);
+
+int N, K;
+int main()
+{
     ifstream fin("angry.in");
     ofstream fout("angry.out");
-    int N, K, max, min; fin >> N >> K;
+    int max, min;
+    fin >> N >> K;
     vector<int> bales(N);
-    for (int x = 0; x<N; x++){
+    for (int x = 0; x < N; x++)
+    {
         fin >> bales[x];
     }
-    max = *max_element(bales.begin(), bales.end());
-    min = *min_element(bales.begin(), bales.end());
-    int answer = (max - min) / (K*2);
-    answer++;
     sort(bales.begin(), bales.end());
-    for (int x = answer-1; x >= 0; x--){
-        int currentDistance = 0, currentCount = 0;
-        for (int y = 0; y<N; y++){
-            if (bales[y] > currentDistance){
-                currentCount++;
-                currentDistance = (x*2) + (bales[y]);
-            }
-            if (currentCount > K){
-                currentCount = -1;
-                break;
-            }
+    int answer = (bales[N - 1] - bales[0]) / (K * 2);
+    answer++;
+    int L = 1, R = answer;
+    while (L <= R)
+    {
+        int mid = L + ((R - L) / 2);
+        if (checkTrue(mid, bales))
+        {
+            answer = mid;
+            R = mid - 1;
         }
-        if (currentCount != -1 && x < answer){
-            answer = x;
-        }
+        else
+            L = mid + 1;
     }
     fout << answer << endl;
+}
+
+bool checkTrue(int x, vector<int> bales)
+{
+    int currentDistance = 0, currentCount = 0;
+    for (int y = 0; y < N; y++)
+    {
+        if (bales[y] > currentDistance)
+        {
+            currentCount++;
+            currentDistance = (x * 2) + (bales[y]);
+        }
+        if (currentCount > K)
+        {
+            currentCount = -1;
+            break;
+        }
+    }
+    if (currentCount != -1)
+    {
+        return true;
+    }
+    return false;
 }
