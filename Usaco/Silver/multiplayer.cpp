74ff156022625f8117678515afc2880b70fba67a
Turtlez200 <88110937+Turtlez200@users.noreply.github.com>
2022-05-14 13:16:16 -0400

    New Problem

--- Usaco/Silver/multiplayer.cpp
+++ Usaco/Silver/multiplayer.cpp
@@ -64,18 +64,12 @@ int main()
 {
     ifstream fin("multimoo.in");
     ofstream fout("multimoo.out");
-    vector<int> players;
     fin >> N;
-
     for (int x = 0; x < N; x++)
     {
         for (int y = 0; y < N; y++)
         {
             fin >> grid[x][y];
-            if (count(players.begin(), players.end(), grid[x][y]) == 0)
-            {
-                players.push_back(grid[x][y]);
-            }
         }
     }
     int max = 0;
@@ -84,23 +78,31 @@ int main()
     {
         for (int y = 0; y < N; y++)
         {
-            int n = dfs(x, y);
-            int p = 0, h = 0;
+            int p = 0, h = 0, n = 0;
+            if (!vis[x][y])
+            {
+                n = dfs(x, y);
+            }
 
             bool vis2[250][250] = {false};
             if (x < N - 1)
             {
                 if (grid[x][y] != grid[x + 1][y])
+                {
                     p = dfs2(x, y, grid[x][y], grid[x + 1][y], vis2);
+                    cout << p << grid[x][y] << grid[x + 1][y] << endl;
+                }
             }
             bool vis3[250][250] = {false};
             if (y < N - 1)
             {
                 if (grid[x][y] != grid[x][y + 1])
+                {
                     h = dfs2(x, y, grid[x][y], grid[x][y + 1], vis3);
+                    cout << h << grid[x][y] << grid[x][y + 1] << endl;
+                }
             }
 
-            cout << h << grid[x][y] << grid[x][y + 1] << endl;
             if (h > max2)
                 max2 = h;
             if (p > max2)
